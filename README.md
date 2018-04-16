74c6687ee1e7f8a842af3aee7484e56f57a8c92c
Alexandra Savramis <alexandra.savramis@gmail.com>
2018-04-15 22:07:33 -0700

    Create outlines for bubble and character

--- README.md
+++ README.md
@@ -1,1 +1,102 @@
-# Bubble-Trouble-Rebubbled
+# Bubble Trouble: Rebubbled
+
+### Background and Overview
+
+Rebubbled is a clone of the classic Bubble Trouble game, in which the character attempts to destroy all the perilous bubbles by dividing them into smaller components and eventually destroying them all together.
+
+### Functionality & MVP  
+
+In Rebubbled, users will be able to:
+
+- [ ] Destroy bubbles of various sizes.
+
+### Functionality & MVP  
+
+In StochAsteroids, users will be able to:
+
+- [ ] Create asteroids of custom size, color, and velocity vector
+- [ ] Hear sounds on collisions
+- [ ] Randomly seed the initial state for demoing or exploration purposes
+- [ ] Start, pause, restart, and reset the simulation
+- [ ] Select from a list of 'instruments' that will serve as the bases for sound events.
+
+This project will include:
+
+- [ ] A control modal describing the basic functionality.
+
+### Wireframes
+
+Rebubbled will be comprised of a single screen,  app will consist of a single screen with the simulation canvas, playback controls, probabilities controls, instrument selectors and nav links to the Github, my LinkedIn, and the About and Details modals.  
+
+The simulation canvas will include a dropdown for selecting the initial color of the creation object. Users will click and drag to create objects of a certain size and vector angle.
+
+Playback controls along the top will include Start, Pause, Restart and Reset buttons.
+
+On the left, there will be an expandable menu allowing the user to control the sound event transition matrix and the color evolution matrix.
+
+On the left near the bottom, will be an expandable menu allowing users to select the 'instruments'.
+
+![wireframes](js_wireframes.png)
+
+### Architecture and Technologies
+
+This project will be implemented with the following technologies:
+
+- Vanilla JavaScript for overall structure and game logic,
+- `HTML5 Canvas` for DOM manipulation and rendering,
+- `Web Audio API` for sound generation, processing and control. `WebAudioAPI` allows for simultaneous sounds with more dependable time triggering
+- Webpack to bundle and serve up the various scripts.
+
+In addition to the webpack entry file, there will be four scripts involved in this project:
+
+`board.js`: this script will handle the logic for creating and updating the necessary DOM elements.
+
+`asteroids.js`: this script will house the physics logic for the asteroids.
+
+`audio.js`: this script will handle the audio logic and the creation of `AudioEvent`s based on the input parameters outlined above. The programming paradigm will be an audio graph consisting of buffers and processing nodes, all connected into a master bus, and referencing a global AudioContext with its own timeline.
+
+`evolutions.js`: this lightweight script will house the constructor and update functions for the `Evolutions` objects.  
+
+### Implementation Timeline
+
+**Over the weekend**:
+- [x] Finished momentum collision physics on my version of the Asteroids project
+- [x] Completed WebAudioAPI Tutorial and loaded basic sound from static assets
+
+**Day 1**: Setup all necessary Node modules, including getting webpack up and running.  Create `webpack.config.js` as well as `package.json`.  Write a basic entry file and the bare bones of all 4 scripts outlined above.  Learn the basics of `Web Audio API`.  Goals for the day:
+
+- [x] Get `webpack` serving files and frame out index.html
+- [x] Learn enough `Web Audio` to render an object to the `Canvas` element and create a sound
+- [x] Port over the relevant pieces of my Asteroids(with collision physics) project and implement Asteroid creation
+
+**Day 2**: Dedicate this day to learning the `Web Audio` API.  First, build out the `AudioEvent` object to connect to the `Board` object.  Then, use `board.js` to create and render `Asteroid`s and `AudioEvent`s. Goals for the day:
+
+- [x] Complete the `asteroids.js` module (constructor, update functions, colors)
+- [x] Get sounds to play on collisions
+- [x] Build first sound library
+- [x] Get collision graphics working
+- [x] Make the `Asteroid`s able to be created with mouse. Set vectors on mouse release.
+
+**Day 3**: Create the logic backend. Build out modular functions for handling the different evolutions. Goals for the day:
+
+- [x] Build Markov Matrix object and link it with collisions
+- [x] Export an `AudioEvent` object with correct type and handling logic
+- [x] Have a functional screen on the `Canvas` frontend that correctly handles creation and running of the simulation.
+- [x] Make sure that starting, stopping, and resetting works.
+
+**Day 4**: Install the controls for the user to interact with the game. Style the frontend, making it polished and professional. Goals for the day:
+
+- [x] Create controls for game speed, stop, start, restart, reset
+- [x] Build out list of instruments
+- [x] Have a styled `Canvas`, nice looking controls and title
+
+
+### Bonus features
+
+There are many directions in which this project could evolve.
+
+- [ ] Alter the Markov matrix governing sound events
+- [ ] Alter the Markov matrix governing color state evolutions
+- [ ] Add recording and saving of output
+- [ ] Allow users to import their own sounds
+- [ ] Allow users to draw obstacles and restrictive spaces into the interaction environment.
---
+++ bundle.js
@@ -0,0 +1,207 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__bubble_js__ = __webpack_require__(1);
+
+
+let canvas = document.getElementById("myCanvas");
+let ctx = canvas.getContext("2d");
+
+let initialBubbleHeight = 100;
+let smallBubble = new __WEBPACK_IMPORTED_MODULE_0__bubble_js__["a" /* default */](
+  10,
+  2.3,
+  0.03,
+  canvas.width/2,
+  canvas.height-30 -initialBubbleHeight,
+  0.4,
+  0,
+  "#FFFF00",
+);
+let bigBubble = new __WEBPACK_IMPORTED_MODULE_0__bubble_js__["a" /* default */](
+  20,
+  4.0,
+  0.03,
+  canvas.width/2 - 20,
+  canvas.height-30 -initialBubbleHeight,
+  0.4,
+  0,
+  "green",
+);
+
+// character
+let characterHeight = 30;
+let characterWidth = 25;
+let characterX = (canvas.width-characterWidth)/2;
+
+// user controllers
+let rightPressed = false;
+let leftPressed = false;
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = true;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = false;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = false;
+    }
+}
+
+function drawCharacter() {
+    ctx.beginPath();
+    ctx.rect(characterX, canvas.height-characterHeight, characterWidth, characterHeight);
+    ctx.fillStyle = "#FF4500";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function draw() {
+  if(rightPressed && characterX < canvas.width-characterWidth) {
+      characterX += 1.25;
+  }
+  else if(leftPressed && characterX > 0) {
+      characterX -= 1.25;
+  }
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawCharacter();
+  smallBubble.moveToNextPos(ctx, canvas);
+  bigBubble.moveToNextPos(ctx, canvas);
+}
+
+setInterval(draw, 3);
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+class Bubble {
+  constructor(radius, initialVelocityY, accelarationFactor, currX, currY, currDX, currDY, color) {
+    this.radius = radius;
+    this.initialVelocityY = initialVelocityY;
+    this.color = color;
+    this.currX = currX;
+    this.currY = currY;
+    this.currDX = currDX;
+    this.currDY = currDY;
+    this.accelarationFactor = accelarationFactor;
+  }
+
+
+  drawBubble(ctx) {
+      ctx.beginPath();
+      ctx.arc(this.currX, this.currY, this.radius, 0, Math.PI*2);
+      ctx.fillStyle = this.color;
+      ctx.fill();
+      ctx.closePath();
+  }
+
+  moveToNextPos(ctx, canvas) {
+    this.currDY += this.accelarationFactor;
+
+    if(this.currX + this.currDX > canvas.width - this.radius || this.currX + this.currDX < this.radius) {
+      this.currDX = -this.currDX;
+    }
+    if(this.currY + this.currDY > canvas.height - this.radius || this.currY + this.currDY < this.radius) {
+      if (this.currDY > 0) {
+        this.currDY = -this.initialVelocityY;
+      } else {
+        this.currDY = this.initialVelocityY;
+      }
+    }
+
+    this.currX += this.currDX;
+    this.currY += this.currDY;
+
+    this.drawBubble(ctx);
+  }
+};
+
+/* harmony default export */ __webpack_exports__["a"] = (Bubble);
+
+
+/***/ })
+/******/ ]);
+//# sourceMappingURL=bundle.js.map
