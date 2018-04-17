bdbbb882d8b33d1acf95adabb5c9a3cce0edd444
Alexandra Savramis <alexandra.savramis@gmail.com>
2018-04-16 21:52:40 -0700

    Extract character into its own class

--- bundle.js
+++ bundle.js
@@ -95,6 +95,7 @@ class Bubble {
     if (this.currX + this.currDX > canvas.width - this.radius || this.currX + this.currDX < this.radius) {
       this.currDX = -this.currDX;
     }
+    
     if (this.currY + this.currDY > canvas.height - this.radius || this.currY + this.currDY < this.radius) {
       if (this.currDY > 0) {
         this.currDY = -this.initialVelocityY;
@@ -116,8 +117,17 @@ class Bubble {
     return Math.sqrt(a * a + b * b);
   }
 
+  checkHorizontalCollision(x, y) {
+    // check that the line's height is greater than (remember the axis
+    // are inversed) the center of the bubble height
+    // check if the line's horizontal distance from the bubble's center is
+    // less than the radius
+
+    return (Math.abs(x - this.currX) < this.radius) && (y < this.currY);
+  }
+
   checkCollison(x, y) {
-    return this.distanceBetween(x, y, this.currX, this.currY) < this.radius;
+    return this.distanceBetween(x, y, this.currX, this.currY) < this.radius || this.checkHorizontalCollision(x, y);
   }
 };
 
@@ -245,21 +255,23 @@ class Laser {
     this.currY = currY;
     this.currDY = 1.5;
     this.currX = currX;
+    this.initialX = currX;
+    this.initialY = currY;
   }
 
   drawLaser(ctx) {
-    ctx.beginPath();
-    ctx.arc(this.currX, this.currY, this.radius, 0, Math.PI*2);
-    ctx.fillStyle = this.color;
-    ctx.fill();
-    ctx.closePath();
-
     // ctx.beginPath();
-    // ctx.moveTo(50,20);
-    // ctx.lineTo(50,100);
-    // ctx.stroke();
+    // ctx.arc(this.currX, this.currY, this.radius, 0, Math.PI*2);
     // ctx.fillStyle = this.color;
+    // ctx.fill();
     // ctx.closePath();
+
+    ctx.beginPath();
+    ctx.moveTo(this.initialX, this.initialY);
+    ctx.lineTo(this.currX, this.currY);
+    ctx.stroke();
+    ctx.fillStyle = this.color;
+    ctx.closePath();
   }
 
   // add characterPos, to shoot
